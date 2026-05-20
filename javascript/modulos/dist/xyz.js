88271def85763f49c0b406a530332d3c0d0973b0
Juan Bernal <bernaljuan081@gmail.com>
2026-05-20 16:54:25 -0600

    producción js

---
+++ javascript/modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(()=>{function o(n){return y("x"),console.log("x"+n),"fin x"}console.log(o("abc"));console.log(o("abx"));console.log(xx("def"));console.log(y("ghi"));console.log(z("jkl"));})();
+//# sourceMappingURL=xyz.js.map
