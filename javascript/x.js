10bbef568c5e21419f35883ce260c3e4bec830b3
Juan Bernal <bernaljuan081@gmail.com>
2026-05-20 16:50:08 -0600

    js con y sin modulos

---
+++ javascript/x.js
@@ -0,0 +1,11 @@
+function x(params) {
+    // y("x")
+    console.log("x"+params)
+    return "fin x"
+}
+
+function xx(params) {
+    return "fin xx"
+}
+
+export {x,xx}
---
+++ javascript/xyz.js
@@ -0,0 +1,6 @@
+import {x} from "./x.js"
+console.log(x("abc"))
+console.log(x("abx"))
+console.log(xx("def"))
+console.log(y("ghi"))
+console.log(z("jkl"))
